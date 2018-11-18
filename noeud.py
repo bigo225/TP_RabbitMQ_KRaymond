1b67b46ced206a2de7c836668a9d1d4cb62c7868
Ange Kragbé <angekragbe@gmail.com>
2018-11-19 00:12:36 +0100

    add queue declare

--- noeud.py
+++ noeud.py
@@ -14,10 +14,14 @@ if len(sys.argv) != 2:
     sys.stderr.write("Usage: %s [node Id] [node holder]\n" % sys.argv[0])
     sys.exit(1)
     
-node_id = sys.argv[1]
-node_holder = sys.argv[2]
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
 
-
+node_id = int(sys.argv[1])
+node_holder = int(sys.argv[2]) if sys.argv[2]!=sys.argv[1] else None
+queue = []
+using=False  ## determine critical section state
+asked=False  ## determine if node has requested for token(privilège)
 #### define variables###
 
 
@@ -32,13 +36,31 @@ node_holder = sys.argv[2]
 
 
 ##### Init
-
-
+def initialize():
+	pass
 
 
 ##### has request
-
-
+def receive_request(int sender):
+	
+	if node_holder == None and using==False and asked==False:
+		node_holder = sender
+		channel.basic_publish(exchange='direct_logs',
+                      routing_key="token",
+                      body="send token")
+		print " [x] Sent token"
+		
+	pass
 
 
 ##### has token
+def receive_token():
+	
+	pass
+
+
+
+
+### Callback
+def callback(ch, method, properties, body):
+    print " [x] %r:%r" % (method.routing_key, body,)
